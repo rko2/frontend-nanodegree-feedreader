ecce13f2e99cc89458bd55c247e15405e281ba55
rko2 <rko2@users.noreply.github.com>
2016-06-09 00:16:26 -0500

    complete RSS Feeds suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,16 +31,28 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('should have a URL', function() {
+          for (var i = 0; i < allFeeds.length; i++) {
+            expect(allFeeds[i].url).toBeDefined();
+          }
+        });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('should have a name', function() {
+           for (var i = 0; i < allFeeds.length; i++) {
+             expect(allFeeds[i].name).toBeDefined();
+           }
+         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe("The menu", function() {
+
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -69,4 +81,5 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+       })
 }());
